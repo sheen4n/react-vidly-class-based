82afa2b2d454caf0b85e53e4fb1fe12b1671374d
sheen4n <sheen.an.goh@gmail.com>
2020-04-04 00:37:02 +0800

    Final Commit for Reference

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,28 +1,28 @@
-import http from "./httpService";
+import http from './httpService';
 
-const apiEndpoint = "/movies";
+const apiEndpoint = '/movies';
 
-function moiveUrl(id) {
-    return `${apiEndpoint}/${id}`;
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
 }
 
 export function getMovies() {
-    return http.get(apiEndpoint);
+  return http.get(apiEndpoint);
 }
 
 export function deleteMovie(movieId) {
-    return http.delete(moiveUrl(movieId));
+  return http.delete(movieUrl(movieId));
 }
 
 export function getMovie(movieId) {
-    return http.get(moiveUrl(movieId));
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
-    if (movie._id) {
-        const body = { ...movie };
-        delete body._id;
-        return http.put(moiveUrl(movie._id), body);
-    }
-    return http.post(apiEndpoint, movie);
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(movieUrl(movie._id), body);
+  }
+  return http.post(apiEndpoint, movie);
 }
